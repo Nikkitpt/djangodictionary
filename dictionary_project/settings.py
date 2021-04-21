a94778a5db5c4b022c098d3192685210c8bc49f0
nicole davis <ndavis213@gmail.com>
2021-04-21 09:05:31 -0400

    ready for heroku deployment

--- dictionary_project/settings.py
+++ dictionary_project/settings.py
@@ -131,3 +131,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Heroku settings
+
+import django_heroku
+django_heroku.settings(locals( ))
